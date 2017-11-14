6a57d23bfdf64398ef9d7a1cc2c94a5afd081fac
tumscorpion <pk.prateep@gmail.com>
2017-11-14 23:28:54 +0700

    test change state

--- config/db.php
+++ config/db.php
@@ -5,8 +5,7 @@ return [
     'dsn' => 'mysql:host=localhost;dbname=yii2basic',
     'username' => 'root',
     'password' => '',
-    'charset' => 'utf8',
-		'test' => 'test'
+    'charset' => 'utf8'
 
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
