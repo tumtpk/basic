0bd197031330d78e33a297e0c6445fa012740562
tumscorpion <pk.prateep@gmail.com>
2017-11-14 22:21:45 +0700

    commit circleci
    Signed-off-by: tumscorpion <pk.prateep@gmail.com>

--- config/db.php
+++ config/db.php
@@ -1,7 +1,14 @@
 <?php
 
 return [
-    'components' => [
-				'mongodb' => [ 'class' => '\yii\mongodb\Connection', 'dsn' => 'mongodb://localhost:27017/wu-dev', ],
-		],
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
 ];
